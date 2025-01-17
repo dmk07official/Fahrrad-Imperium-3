a5ac8dc68d3ca7f6d6522b973a137125e94f90c5
dmk07official <122459984+dmk07official@users.noreply.github.com>
2025-01-17 12:31:24 +0100

    Update game.js

--- game/game.js
+++ game/game.js
@@ -500,7 +500,7 @@ function handlePlayerClick() {
   playerDiv.querySelector("p:nth-of-type(1)").textContent = `Progress: ${progress}/${job.work}`;
 
   playerDiv.style.transition = "background-color 0.5s ease";
-  playerDiv.style.backgroundColor = "#605a3a";
+  playerDiv.style.backgroundColor = "#4d4b41";
 
   // Falls bereits ein Timeout läuft, wird es zurückgesetzt
   if (timeoutId) {
