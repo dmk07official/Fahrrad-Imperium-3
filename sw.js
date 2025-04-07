3da2073a2415f1e5814bab5a57c9dabc04fc8ed6
dmk07official <122459984+dmk07official@users.noreply.github.com>
2025-04-07 17:16:15 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'my-game-cache-v1';
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
